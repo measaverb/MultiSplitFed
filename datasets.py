2113c0a30a8b8b08c2465461165922231278ab2d
measaverb <measaverb@gachon.ac.kr>
2024-09-27 18:07:37 +0900

    Initial commit

---
+++ datasets.py
@@ -0,0 +1,60 @@
+import os
+import pandas as pd
+import numpy as np
+
+from glob import glob
+from PIL import Image
+from torch.utils.data import Dataset
+from sklearn.model_selection import train_test_split
+
+from utils import get_transforms
+
+
+class HAM10000Dataset(Dataset):
+    def __init__(self, df, transforms=None):
+        self.df = df
+        self.transforms = transforms
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        image_path = self.df["path"][idx]
+        image = Image.open(image_path)
+        label = self.df["target"][idx]
+
+        if self.transforms:
+            image = self.transforms(image)
+
+        return image, label
+
+
+def get_datasets(metadata_path, num_users):
+    df = pd.read_csv(metadata_path)
+    lesion_type = {
+        "nv": "Melanocytic nevi",
+        "mel": "Melanoma",
+        "bkl": "Benign keratosis-like lesions ",
+        "bcc": "Basal cell carcinoma",
+        "akiec": "Actinic keratoses",
+        "vasc": "Vascular lesions",
+        "df": "Dermatofibroma",
+    }
+    imageid_path = {
+        os.path.splitext(os.path.basename(x))[0]: x
+        for x in glob(os.path.join("data", "*", "*.jpg"))
+    }
+
+    df["path"] = df["image_id"].map(imageid_path.get)
+    df["cell_type"] = df["dx"].map(lesion_type.get)
+    df["target"] = pd.Categorical(df["cell_type"]).codes
+
+    train, test = train_test_split(df, test_size=0.2)
+
+    train = train.reset_index()
+    test = test.reset_index()
+
+    train_transforms, test_transforms = get_transforms()
+
+    dataset_train = HAM10000Dataset(train, transform=train_transforms)
+    dataset_test = HAM10000Dataset(test, transform=test_transforms)
---
+++ utils.py
@@ -0,0 +1,47 @@
+import copy
+import numpy as np
+import torch
+from torchvision import transforms
+
+
+def get_transforms():
+    mean = [0.485, 0.456, 0.406]s
+    std = [0.229, 0.224, 0.225]
+    train_transforms = transforms.Compose(
+        [
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            transforms.Pad(3),
+            transforms.RandomRotation(10),
+            transforms.CenterCrop(64),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean, std=std),
+        ]
+    )
+    test_transforms = transforms.Compose(
+        [
+            transforms.Pad(3),
+            transforms.CenterCrop(64),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean, std=std),
+        ]
+    )
+    return train_transforms, test_transforms
+
+
+def dataset_iid(dataset, num_users):
+    num_items = int(len(dataset) / num_users)
+    dict_users, all_idxs = {}, [i for i in range(len(dataset))]
+    for i in range(num_users):
+        dict_users[i] = set(np.random.choice(all_idxs, num_items, replace=False))
+        all_idxs = list(set(all_idxs) - dict_users[i])
+    return dict_users
+
+
+def federated_averaging(w):
+    w_avg = copy.deepcopy(w[0])
+    for k in w_avg.keys():
+        for i in range(1, len(w)):
+            w_avg[k] += w[i][k]
+        w_avg[k] = torch.div(w_avg[k], len(w))
+    return w_avg
